39cce11168b5007db5a5a7dfba29ebbc1da6d5fb
zachary0447 <82210447@qq.com>
2017-11-23 20:04:27 +0800

    drawing board v0.2

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
-// 鼠标按下去
-var div = document.getElementById('canvas')
+var yyy = document.getElementById('xxx');
+var pageWidth = document.documentElement.clientWidth
+var pageHeight = document.documentElement.clientHeight
+yyy.width = pageWidth
+yyy.height = pageHeight
+
+window.onresize = function(){
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+    
+    yyy.width = pageWidth
+    yyy.height = pageHeight
+        
+}
+
+var context = yyy.getContext('2d');
+
+
+function darwCircle(x, y, radius) {
+    context.beginPath()
+    context.arc(x, y, radius, 0, Math.PI * 2)
+    context.fill()
+}
+
 var painting = false
+var lastPoint = {x:undefined, y:undefined}
 
-div.onmousedown = function (a) {
+yyy.onmousedown = function (aaa) {
     painting = true
-    var x = a.clientX
-    var y = a.clientY
-    var divA = document.createElement('div')
-    divA.style = "width: 6px; height: 6px;" + "background: black; border-radius: 3px;" + "position:absolute; left: " + (x - 3) + "px;" + "top: " + (y - 3) + "px;"
-    div.appendChild(divA)
+    var x = aaa.clientX
+    var y = aaa.clientY
+    lastPoint = {'x':x,'y':y}
 }
 
-// 动鼠标
-div.onmousemove = function (a) {
-    if (painting){
-        var x = a.clientX
-        var y = a.clientY
-        var divA = document.createElement('div')
-        divA.style = "width: 6px; height: 6px;" + "background: black; border-radius: 3px;" + "position:absolute; left: " + (x - 3) + "px;" + "top: " + (y - 3) + "px;"
-        div.appendChild(divA)
+yyy.onmousemove = function (aaa) {
+    if(painting){
+        var x = aaa.clientX
+        var y = aaa.clientY
+        var newPoint = {"x":x, "y":y}
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
     }
-}
-// 松开鼠标
-div.onmouseup = function (a) {
-    painting = false
 
 }
+yyy.onmouseup = function (aaa) {
+    painting = false
+}
+
+function drawLine (x1, y1, x2, y2){
+    context.beginPath();
+    context.moveTo(x1, y1) // 起点
+    context.lineWidth = 5
+    context.lineTo(x2, y2)
+    context.stroke()
+    context.closePath()
+}
+
